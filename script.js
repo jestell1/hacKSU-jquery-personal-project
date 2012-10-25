54d6347276cab8ad9d91572b1b9296836a39b801
jestell1 <jestell1@kent.edu>
2012-10-24 23:54:34 -0400

    Cleaned Up Code and Made more internal Comments

--- script.js
+++ script.js
@@ -16,20 +16,18 @@ function voteSnack({
 
 });
 
-//Check the List of emails
-function checkIPAddress({
-//Get IP address
 
-//If exists go to voted page
-
-//Else create new IP address and show voting page
-});
 
 
 $(document).ready( function() {
-    $.getJSON( "http://smart-ip.net/geoip-json?callback=?",
+//Get IP address
+	$.getJSON( "http://smart-ip.net/geoip-json?callback=?",
         function(data){
             alert (data.host);
         }
     );
+
+//If exists go to voted page
+
+//Else create new IP address and show voting page
 });
